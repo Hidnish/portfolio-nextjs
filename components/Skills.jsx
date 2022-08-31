2c9372b0e19196901f6ff0f536cf3b29ebfcac92
Hidnish <hideonishimura@live.it>
2022-08-31 16:46:05 +0200

    Add skills pop up

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -9,109 +9,36 @@ import Python from '../public/assets/skills/python.png';
 import Bootstrap from '../public/assets/skills/bootstrap.png';
 import EditorX from '../public/assets/skills/editorx.png';
 import Mongo from '../public/assets/skills/mongo.png';
+import SkillTag from './subComponents/SkillTag';
 
 const Skills = () => {
 	return (
-		<div id="skills" className="p-2 w-[90%] md:w-[85%] lg:h-[100vh] mx-auto flex items-center">
-			<div className="max-w-[1240px] flex flex-col justify-center lg:grid grid-cols-12 gap-8 ">
-				<div className="col-span-1 flex items-center lg:justify-center rounded-full lg:bg-gradient-to-b from-[#add2e6] to-[#bd95e8]">
-					<p className="uppercase text-xl tracking-widest font-extrabold text-[#57b099] lg:text-[#ecf0f3] lg:rotate-[-90deg]">
+		<div
+			id="skills"
+			className="p-2 w-[90%] md:w-[85%] lg:h-[100vh] mx-auto flex flex-col justify-center"
+		>
+			<div className="max-w-[1240px] lg:grid grid-cols-12 lg:gap-8">
+				<div className="col-span-1 lg:flex lg:items-center lg:justify-center lg:border-r lg:border-oltremare lg:mr-3">
+					<p className="uppercase text-xl tracking-widest font-extrabold text-oltremare lg:rotate-[-90deg] pb-3">
 						Skills
 					</p>
 				</div>
 				<div className="col-span-11">
-					<h2 className="py-4">What I Can Do</h2>
-					<div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8">
-						<div className="flex justify-center p-6 drop-shadow rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={Html}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
-						<div className="flex justify-center p-6 shadow-md rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={Css}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
-						<div className="flex justify-center p-6 shadow-md rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={Javascript}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
-						<div className="flex justify-center p-6 shadow-md rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={ReactImg}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
-						<div className="flex justify-center p-6 shadow-md rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={Tailwind}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
-						<div className="flex justify-center p-6 shadow-md rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={Bootstrap}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
-						<div className="flex justify-center p-6 shadow-md rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={Python}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
-						<div className="flex justify-center p-6 shadow-md rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={Mongo}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
-						<div className="flex justify-center p-6 shadow-md rounded-full h-32 w-32 hover:scale-105 ease-in duration-300">
-							<div className="m-auto">
-								<Image
-									src={EditorX}
-									width="64px"
-									height="64px"
-									alt="/"
-								/>
-							</div>
-						</div>
+					<h2 className="py-4 lg:pb-10 lg:pt-0">What I Can Do</h2>
+					<div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-8">
+						<SkillTag img={Html} title="HTML" />
+						<SkillTag img={Css} title="CSS" />
+						<SkillTag
+							img={Javascript}
+							title="JavaScript"
+							radius={10}
+						/>
+						<SkillTag img={ReactImg} title="React" />
+						<SkillTag img={Tailwind} title="Tailwindcss" />
+						<SkillTag img={Bootstrap} title="Bootstrap" />
+						<SkillTag img={Python} title="Python" />
+						<SkillTag img={Mongo} title="MongoDB" />
+						<SkillTag img={EditorX} title="EditorX" radius={10} />
 					</div>
 				</div>
 			</div>
