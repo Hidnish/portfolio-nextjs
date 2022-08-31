22193edc94096350d4e886f3044fbf2d8c0c9632
Hidnish <hideonishimura@live.it>
2022-08-31 16:44:57 +0200

    Set up hook to determine which section is shown in the viewport

--- hooks/useScrollPosition.js
+++ hooks/useScrollPosition.js
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export const useScrollPosition = () => {
-  const [scrollPos, setScrollPos] = useState(false);
+	const [scrollPos, setScrollPos] = useState(false);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", () => {
-        if (window.pageYOffset > 50) {
-          setScrollPos(true);
-        } else {
-          setScrollPos(false);
-        }
-      });
-    }
-  }, []);
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			window.addEventListener('scroll', () => {
+				if (window.pageYOffset > 50) {
+					setScrollPos(true);
+				} else {
+					setScrollPos(false);
+				}
+			});
+		}
+	}, []);
 
-  return scrollPos ;
+	return scrollPos;
 };
