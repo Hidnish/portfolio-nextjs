13c5f3313a359cfd41856d79d1efbae7f02740b6
Hidnish <hideonishimura@live.it>
2022-08-31 16:46:34 +0200

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'polvere': '#24292E', 
+        'bamba': '#ecf0f3', 
+        'grigello': '#888888',
+        'linkedin': '#0072b1', 
+        'github': '#24292E',
+        'oltremare': '#57b099',
+        'azzurrino': '#add2e6',
+        'rosino': '#bd95e8'
+      }
+    },
   },
   plugins: [],
 }
