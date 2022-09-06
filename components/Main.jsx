616723623a578decd2bd64f7280e7e3e6d78b2eb
Hidnish <hideonishimura@live.it>
2022-09-06 14:22:51 +0200

    Change text

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,46 +1,53 @@
 import React from 'react';
-import { AiOutlineMail } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsPersonLinesFill } from 'react-icons/bs';
-import { HiOutlineMail } from 'react-icons/hi';
 
 const Main = () => {
 	return (
-		<section id="main" className="w-full h-screen text-center bg-gradient-to-b from-[#c0e9ff] to-[#d0a4ff] text-polvere">
+		<section
+			id="main"
+			className="w-full h-screen text-center bg-gradient-to-b from-[#c0e9ff] to-[#d0a4ff] text-polvere"
+		>
 			<div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
 				<div>
 					<p className="py-3 text-sm tracking-widest text-gray-600">
-						Let&apos;s Build Something Great
+						Let&apos;s Build Something Great Together
 					</p>
 					<h1 className="pb-0 lg:py-2 text-gray-700">
 						Hi I&apos;m{' '}
 						<span className="text-oltremare">Hideo</span>,
 					</h1>
-					<h1 className="py-2 text-gray-700">Junior Full Stack Web Developer</h1>
+					<h1 className="py-2 px-2 text-gray-700">
+						Junior Full Stack Web Developer
+					</h1>
 					<p className="py-6 text-gray-600 max-w-[70%] m-auto m-b-[100px]">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-						sed do eiusmod tempor incididunt ut labore et dolore
-						magna aliqua. Ut enim ad minim veniam, quis nostrud
-						exercitation ullamco laboris nisi ut aliquip ex ea
-						commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu
-						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-						non proident, sunt in culpa qui officia deserunt mollit
-						anim id est laborum.
+						Welcome to my official Portfolio Website. Please take a
+						look around and don&apos;t hesitate to contact me for
+						collaborations and new project ideas!
 					</p>
 					<div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-							<div className="rounded-full p-3 cursor-pointer hover:bg-[#0072b1] hover:text-white ease-in duration-200">
-								<FaLinkedinIn size={20}/>
-							</div>
-							<div className="rounded-full p-3 cursor-pointer hover:bg-github hover:text-white ease-in duration-200">
-								<FaGithub size={20}/>
-							</div>
-							<div className="rounded-full p-3 cursor-pointer hover:bg-amber-500 hover:text-white ease-in duration-200">
-								<HiOutlineMail size={20}/>
-							</div>
-							<div className="rounded-full p-3 cursor-pointer hover:bg-[#C85250] hover:text-white ease-in duration-200">
-								<BsPersonLinesFill size={20}/>
-							</div>
+						<a
+							href="https://www.linkedin.com/in/hideo-tommaso-nishimura-5029b0159/"
+							target="_blank"
+							rel="noreferrer"
+							className="rounded-full p-3 cursor-pointer hover:bg-[#0072b1] hover:text-white ease-in duration-200"
+						>
+							<FaLinkedinIn size={20} />
+						</a>
+						<a
+							href="https://github.com/Hidnish"
+							target="_blank"
+							rel="noreferrer"
+							className="rounded-full p-3 cursor-pointer hover:bg-github hover:text-white ease-in duration-200"
+						>
+							<FaGithub size={20} />
+						</a>
+						<a
+							href="#contacts"
+							className="rounded-full p-3 cursor-pointer hover:bg-[#C85250] hover:text-white ease-in duration-200"
+						>
+							<BsPersonLinesFill size={20} />
+						</a>
 					</div>
 				</div>
 			</div>
