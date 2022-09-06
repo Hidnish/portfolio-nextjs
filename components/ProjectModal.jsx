2ef91418530e4533058b4549b517da5bdf20b8cc
Hidnish <hideonishimura@live.it>
2022-09-06 14:23:19 +0200

    Change text size

--- components/ProjectModal.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ProjectModal = () => {
-	return (
-        <div className="fixed left-0 top-0 bg-[rgba(0,0,0,0.1)] h-[100vh] w-[100vw] z-[100]">ProjectModal</div>
-    );
-};
-
-export default ProjectModal;
